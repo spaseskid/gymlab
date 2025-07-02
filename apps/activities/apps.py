e342e1b9ddbb6337c40a01fe54ab89a32dda41be
spaseskid <spaseskid7@gmail.com>
2025-07-02 02:56:31 +0200

    Configure Django apps/settings

--- apps/activities/apps.py
+++ apps/activities/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ActivitiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'activities'
+    name = 'apps.activities'
